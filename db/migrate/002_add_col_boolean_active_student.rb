685b54d14eb2b11d6a505a1820368fade32f1c13
Eric McKinney <mckinney99@gmail.com>
2018-01-12 17:54:35 +0000

    automatically backed up by learn

---
+++ db/migrate/002_add_col_boolean_active_student.rb
@@ -0,0 +1,8 @@
+class CreateStudents < ActiveRecord::Migration
+  def up
+  add_column :students, :active, :boolean, default: false
+end
+
+  def down
+    add_column :students, :active, :boolean, default: true
+  end
